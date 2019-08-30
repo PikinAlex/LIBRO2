c9569401e9e3ce3d6fc6673dc860c8e325ad3a6b
PikinAlex <alexis_usiel@hotmail.com>
2019-08-30 14:52:59 -0600

    Desestructuracion

--- ejemplo.js
+++ ejemplo.js
@@ -1,4 +1,11 @@
-var add = function (x) { return function (y) { return x + y; }; };
-add(123)(456);
-var add123 = add(123);
-add123(456);
+var obj = { x: 1, y: 2, z: 3 };
+console.log(obj.x);
+var x = obj.x, y = obj.y, z = obj.z;
+console.log(x);
+var array = [1, 2, 3];
+console.log(array[0]);
+var x = array[0], y = array[1], z = array[2];
+console.log(x);
+var array = [1, 2, 3, 4];
+var x = array[0], y = array[1], rest = array.slice(2);
+console.log(x, y, rest);
--- ejemplo.ts
+++ ejemplo.ts
@@ -1,4 +1,15 @@
-let add=(x: number )=> (y: number) => x+y;
-add(123)(456);
-let add123= add(123);
-add123(456);
+var obj ={x:1, y:2, z:3};
+console.log(obj.x);
+
+var{x,y,z} = obj;
+console.log(x);
+
+var array = [1,2,3];
+console.log(array[0]);
+
+var [x,y,z] = array;
+console.log(x);
+
+var array = [1,2,3,4];
+var [x,y, ...rest]=array;
+console.log(x,y,rest);
