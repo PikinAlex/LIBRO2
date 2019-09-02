f6fc31f54f41283cb2807f24a1b90e8507741434
PikinAlex <alexis_usiel@hotmail.com>
2019-09-01 23:51:09 -0600

    con json

--- ejemplo.ts
+++ ejemplo.ts
@@ -1,12 +1,21 @@
-function iReturnPromiseAfter1Second():Promise<string> {
-    return new Promise((resolve)=>{
-    setTimeout(()=>resolve("Hello world!"), 1000);
-    });
-    }
-    Promise.resolve(123)
-    .then((res)=>{
-    return iReturnPromiseAfter1Second();
-    })
-    .then((res) => {
-    console.log(res);
-    });
+loadJSONAsync('good.json')
+.then(function (val) { console.log(val); })
+.catch(function (err) {
+console.log('good.json error', err.message);
+})
+// non-existent json file
+.then(function () {
+return loadJSONAsync('absent.json');
+})
+.then(function (val) { console.log(val); })
+.catch(function (err) {
+console.log('absent.json error', err.message);
+})
+// invalid json file
+.then(function () {
+return loadJSONAsync('bad.json');
+})
+.then(function (val) { console.log(val); })
+.catch(function (err) {
+console.log('bad.json error', err.message);
+});
