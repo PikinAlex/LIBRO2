fde9ffd05f16afbe3cebb1925ced98a35f0fa8bd
PikinAlex <alexis_usiel@hotmail.com>
2019-09-01 22:57:24 -0600

    Estructuracion

--- ejemplo.js
+++ ejemplo.js
@@ -1,11 +1,9 @@
-var obj = { x: 1, y: 2, z: 3 };
-console.log(obj.x);
-var x = obj.x, y = obj.y, z = obj.z;
-console.log(x);
-var array = [1, 2, 3];
-console.log(array[0]);
-var x = array[0], y = array[1], z = array[2];
-console.log(x);
-var array = [1, 2, 3, 4];
-var x = array[0], y = array[1], rest = array.slice(2);
-console.log(x, y, rest);
+function rest(first, second) {
+    var allOthers = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        allOthers[_i - 2] = arguments[_i];
+    }
+    console.log(allOthers);
+}
+rest('foo', 'bar');
+rest('foo', 'bar', 'bas', 'qux');
