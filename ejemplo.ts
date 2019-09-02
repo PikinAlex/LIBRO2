bdd27360423b50e4b62ab7a218363f170be928dc
PikinAlex <alexis_usiel@hotmail.com>
2019-09-01 23:35:36 -0600

    cadenas promeas con catch

--- ejemplo.ts
+++ ejemplo.ts
@@ -1,13 +1,16 @@
-Promise.resolve(123)
+Promise.reject(new Error('something bad happened'))
 .then((res) => {
-console.log(res); 
+console.log(res);
 return 456;
 })
 .then((res) => {
-console.log(res);
+console.log(res); 
 return Promise.resolve(123);
 })
 .then((res) => {
 console.log(res); 
 return Promise.resolve(123);
 })
+.catch((err) => {
+console.log(err.message); 
+});
