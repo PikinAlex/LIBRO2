a257dcc71d763d863107921433a11179038f5240
PikinAlex <alexis_usiel@hotmail.com>
2019-09-02 23:54:22 -0600

    interfaces

--- ejemplo.js
+++ ejemplo.js
@@ -1,36 +1,7 @@
-var Disposable = /** @class */ (function () {
-    function Disposable() {
+var test = /** @class */ (function () {
+    function test(b, c) {
+        this.b = b;
+        this.c = c;
     }
-    Disposable.prototype.dispose = function () {
-        this.isDisposed = true;
-    };
-    return Disposable;
+    return test;
 }());
-var Activatable = /** @class */ (function () {
-    function Activatable() {
-    }
-    Activatable.prototype.activate = function () {
-        this.isActive = true;
-    };
-    Activatable.prototype.deactivate = function () {
-        this.isActive = false;
-    };
-    return Activatable;
-}());
-;
-var SmartObject = /** @class */ (function () {
-    function SmartObject() {
-        this.isDisposed = false;
-        this.isActive = false;
-    }
-    return SmartObject;
-}());
-;
-function applyMixins(derivedCtor, baseCtors) {
-    baseCtors.forEach(function (baseCtor) {
-        Object.getOwnPropertyNames(baseCtor.prototype).forEach(function (name) {
-            derivedCtor.prototype[name] = baseCtor.prototype[name];
-        });
-    });
-}
-applyMixins(SmartObject, [Disposable, Activatable]);
--- ejemplo.ts
+++ ejemplo.ts
@@ -1,33 +1,14 @@
-class Disposable {
-    isDisposed: boolean;
-    dispose() {
-    this.isDisposed = true;
+interface a {
+    b: number;
+    }
+    interface b extends a {
+    c: string;
+    }
+    class test implements b {
+    b: number;
+    c: string;
+    constructor (b: number, c: string) {
+    this.b = b;
+    this.c = c;
     }
     }
-
-    class Activatable {
-    isActive: boolean;
-    activate() {
-    this.isActive = true;
-    }
-    deactivate() {
-    this.isActive = false;
-    }
-};
-
-class SmartObject implements Disposable, Activatable {
-    isDisposed: boolean = false;
-    dispose: () => void; 
-    isActive: boolean = false;
-    activate: () => void;
-    deactivate: () => void;
-};
-function applyMixins(derivedCtor: any, baseCtors: any[]) {
-    baseCtors.forEach(baseCtor => {
-    Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-    derivedCtor.prototype[name] = baseCtor.prototype[name];
-    });
-    });
-    }
-
-    applyMixins(SmartObject, [Disposable, Activatable]);
