0cd6be6d269690859fb888e7a09c950441021e45
PikinAlex <alexis_usiel@hotmail.com>
2019-09-02 22:22:42 -0600

    ejemplo de clases

--- ejemplo.ts
+++ ejemplo.ts
@@ -1,12 +1,45 @@
-class Startup {
-    private text: String;
-    constructor (texto: String) {
-    this.text = texto;
+class Calculo {
+    
+    private x: number = 0;
+    private y: number = 0;
+    
+    constructor (x: number, y: number){
+    
+    this.setX(x);
+    this.setY(y);
     }
-    public main () : number {
-    console.log(this.text);
-    return 0;
+    
+    public setX(x: number) : void{
+    this.x = x;
+    }
+    public setY(y: number) : void{
+    this.y = y;
+    }
+    
+    public getX(): number {
+    return this.x;
+    }
+    public getY(): number {
+    return this.y;
+    }
+    public sumar() : number {
+    return (this.getX() + this.getY());
+    }
+    public restar() : number{
+    return ( this.mayor() - this.menor() );
+    }
+    public menor() : number {
+    if(this.getX() >= this.getY()) {
+    return this.getY();
+    }
+    return this.getX();
+    }
+    public mayor() : number {
+    if(this.getX() >= this.getY()) {
+    return this.getX();
+    }
+    return this.getY();
     }
 }
-    let s = new Startup("Hola mundo");
-    s.main();
+let calculo = new Calculo(30,10);
+console.log(calculo.restar());
