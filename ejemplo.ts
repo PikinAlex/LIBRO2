714f1960661a07a66f57b1dc4cd79276a6606b4e
PikinAlex <alexis_usiel@hotmail.com>
2019-09-02 23:03:25 -0600

    IIFE

--- ejemplo.ts
+++ ejemplo.ts
@@ -1,10 +1,26 @@
-class Something {
-    static instances = 0;
-    constructor() {
+class Point {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    }
+    add(point: Point) {
+    return new Point(this.x + point.x, this.y + point.y);
+    }
+    }
     
-    Something.instances++;
+    class Point3D extends Point {
+    z: number;
+    constructor(x: number, y: number, z: number) {
+    super(x, y);
+    this.z = z;
+    }
+    
+    add(point: Point3D) {
+    
+    var point2D = super.add(point);
+    
+    return new Point3D(point2D.x, point2D.y, this.z + point.z);
     }
     }
-    var s1 = new Something();
-    var s2 = new Something();
-    console.log(Something.instances);
