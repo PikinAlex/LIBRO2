8318234ab859be0bca18628ed0875fc60ec121b3
PikinAlex <alexis_usiel@hotmail.com>
2019-09-02 22:25:34 -0600

    ejemplo grafico

--- ejemplo.ts
+++ ejemplo.ts
@@ -1,45 +1,16 @@
-class Calculo {
-    
-    private x: number = 0;
-    private y: number = 0;
-    
-    constructor (x: number, y: number){
-    
-    this.setX(x);
-    this.setY(y);
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+    this.greeting = message;
     }
-    
-    public setX(x: number) : void{
-    this.x = x;
+    greet() {
+    return "Hello, " + this.greeting;
     }
-    public setY(y: number) : void{
-    this.y = y;
     }
-    
-    public getX(): number {
-    return this.x;
+    let greeter = new Greeter("world");
+    let button = document.createElement('button');
+    button.textContent = "Say Hello";
+    button.onclick = function() {
+    alert(greeter.greet());
     }
-    public getY(): number {
-    return this.y;
-    }
-    public sumar() : number {
-    return (this.getX() + this.getY());
-    }
-    public restar() : number{
-    return ( this.mayor() - this.menor() );
-    }
-    public menor() : number {
-    if(this.getX() >= this.getY()) {
-    return this.getY();
-    }
-    return this.getX();
-    }
-    public mayor() : number {
-    if(this.getX() >= this.getY()) {
-    return this.getX();
-    }
-    return this.getY();
-    }
-}
-let calculo = new Calculo(30,10);
-console.log(calculo.restar());
+    document.body.appendChild(button);
